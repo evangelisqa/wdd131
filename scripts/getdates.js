3b169fb85b399cc13c3a0050d8dd0289a90bae7d
evangelisqa <albesaevan@icloud.com>
2024-06-17 01:29:57 -0700

    a

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,14 @@
+
 document.addEventListener('DOMContentLoaded',
 (event) =>{
+    // Get current year
+    const currentYear = newDate().getFullYear();
+    // Set the copyright year
+    document.querySelector('footer p:first-of-type').textContent = `Copryright @${currentYear}`;
+    const lastModifiedDate = document.lastModified;
+    document.querySelector(`footer p:nth-of-type(2)`).textContent = `Last Updated: ${lastModifiedDate}`
+
+
     document.getElementById('currentYear').textContent=new Date().getFullYear;
     document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
 });
