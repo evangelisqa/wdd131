7072735f29e98077faf6093fdc2f69165f2b5051
evangelisqa <albesaevan@icloud.com>
2024-06-17 17:15:22 -0700

    add

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,14 +1,7 @@
 
-document.addEventListener('DOMContentLoaded',
-(event) =>{
-    // Get current year
-    const currentYear = newDate().getFullYear();
-    // Set the copyright year
-    document.querySelector('footer p:first-of-type').textContent = `Copryright @${currentYear}`;
-    const lastModifiedDate = document.lastModified;
-    document.querySelector(`footer p:nth-of-type(2)`).textContent = `Last Updated: ${lastModifiedDate}`
+const currentYear = new Date().getFullYear;
 
+const lastModifiedDate = documennt.lastModified;
 
-    document.getElementById('currentYear').textContent=new Date().getFullYear;
-    document.getElementById('lastModified').textContent = `Last Modified: ${document.lastModified}`;
-});
+document.querySelector('footer p:first-of type').textContent = `Copyright @ $ {currentYear}`;
+document.querySelector('footer p:nth-of-type(2)').textContent = `Last Modified: $ {lastModifiefDate}`;
