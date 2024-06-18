a886288587323f8179a18441048b9285eb795d61
evangelisqa <albesaevan@icloud.com>
2024-06-17 17:28:17 -0700

    add

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,4 +4,4 @@ const currentYear = new Date().getFullYear;
 const lastModifiedDate = documennt.lastModified;
 
 document.querySelector('footer p:first-of type').textContent = `Copyright @ ${currentYear}`;
-document.querySelector('footer p:nth-of-type(2)').textContent = `Last Modified: ${lastModifiefDate}`;
+document.querySelector('footer p:nth-of-type(2)').textContent = `Last Modified: ${lastModifiedDate}`;
