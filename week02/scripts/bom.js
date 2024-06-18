40b6f4dfcb432ce90d8962c2a5ea5da79af4441c
evangelisqa <albesaevan@icloud.com>
2024-06-17 21:48:59 -0700

    add

---
+++ week02/scripts/bom.js
@@ -0,0 +1,9 @@
+const input = Document.querySelector('@favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('____'); //fill up the blank
+const li = document.createElement('li');
+const deleteButton = Document.createElement('button');
+li.textContent = input.value;
+deleteButton.textContent = 'X';
+li.append(eleteButton);
+list.append(li);
