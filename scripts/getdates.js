41f3f33f5645c45bb2eaae65e4e5603b22c0a4b2
evangelisqa <albesaevan@icloud.com>
2024-06-22 16:26:01 -0700

    add

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,6 +4,6 @@ const currentyear = new Date("2024-06-22T12:00:00z").getFullYear(2024);
 document.querySelector("footer p:first-child span").textContent = currentYear;
 const lastModified = new Date(document.lastModified);
 const options = {year:'numeric', month:'long', day:'numeric'};
-const formattedLastModified = lastModified.toLocaleDateString('en-US', options);
+const formattedLastModified = lastModified.toLocaleDateString('en-CAN', options);
 
 document.getElementById("lastModified").textContent =`Last modified:${formattedLastModified}`;
