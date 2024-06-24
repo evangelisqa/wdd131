bd2e812db22f81db46c3bd0a21593137b0305893
evangelisqa <albesaevan@icloud.com>
2024-06-23 22:17:06 -0700

    a

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,9 +1,4 @@
 
-const currentyear = new Date("2024-06-22T 00:00:00").getFullYear(2024);
+const date = new Date();
 
-const lastModifiedDate = new Date().toLocaleDateString();
-
-document.getElementById('currentyear').textContent = currentYear;
-
-document.getElementById('lastModified').textContent = `Last modofied:${lastModifiedDate}`;
-
+console.log(date);
