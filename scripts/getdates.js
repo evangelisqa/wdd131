3892d1af375f0c5966703a729c1f0c59943a0f91
evangelisqa <albesaevan@icloud.com>
2024-06-24 01:14:54 -0700

    a

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,10 @@
 
-const date = new Date();
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
 
-console.log(date);
+const lastModifiedDate = new Date(document.lastModified);
+const options = {year: 'numeric', month: 'long', day:'numeric'};
+
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModifiedDate.toLocaleDateString (undefined, options)}`;
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModifiedDate.toLocaleTimeString (undefined, options)}`;
+
---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModifiedDate = new Date(document.lastModified);
+const options = {year: 'numeric', month: 'long', day:'numeric'};
+
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModifiedDate.toLocaleDateString (undefined, options)}`;
+document.getElementById("lastModified").textContent = `Last Modified: ${lastModifiedDate.toLocaleTimeString (undefined, options)}`;
