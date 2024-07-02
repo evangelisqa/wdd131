55c4e0160816731df22424ac5f25ba96f34a478c
evangelisqa <albesaevan@icloud.com>
2024-07-02 00:37:35 -0700

    add

--- scripts/place.js
+++ scripts/place.js
@@ -1,16 +1,26 @@
-const weatherData = {
-    temperature: "60°F",
-    condition: "Mostly Clear",
-    windSpeed: "10 mph",
-    windhill: "-5°F"
-};
+//To calculate windchill
+function calculateWindChill(temperature, windSpeed) {
+    if (temperature <= 10 && windSpeed > 4.8) {
+        return Math.round(13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16));
+    }
+    else{
+        return 'N/A';
+    }
+}
 
-document.getElementById("temperature").textContent = weatherData.temperature;
-document.getElementById("Condition").textContent = weatherData.condition;
-document.getElementById("winsSpeed").textContent = weatherData.windSpeed;
-document.getElementById("windChill").textContent = weatherData.windchill;
+// Notify the windchill value on page load
+window.onload = function(){
+    //Static values for temp. and wind speed
+    var temperature = 30; // in Celsius
+    var windSpeed = 10; // in km/h
 
+    //To calculate wind chill
+    var windChill = calculateWindChill(temperature, windSpeed);
 
+    // To display wind chill
+    var windChillValue = document.getElementById('wind-chill-value');
+    windChillValue.textContent = windChill;
+}
 
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
