9de2f1899e930c8fcb54067cd02c8beb84a70426
evangelisqa <albesaevan@icloud.com>
2024-07-06 18:11:29 -0700

    add

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,4 +1,4 @@
-const hamButton = document.querySelector('#menu');
+const hamButton = document.querySelector('#hambutton');
 const navigation = document.querySelector('nav');
 // select menu buttons
 const homeButton = document.querySelector('#home-button');
