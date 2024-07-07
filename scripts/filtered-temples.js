7dd2f3072eaa43c396b05c86cfcbc82cbcb59a50
evangelisqa <albesaevan@icloud.com>
2024-07-07 01:07:08 -0700

    add

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -9,7 +9,8 @@ const smallButton = document.querySelector('#small-button');
 
 const pageHeading = document.querySelector('#page-heading');
 
-hamButton.addEventListener('click',()=> {
+
+hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
@@ -17,84 +18,176 @@ hamButton.addEventListener('click',()=> {
 
 const temples = [
     {
-      templeName: "Aba Nigeria",
-      location: "Aba, Nigeria",
-      dedicated: "2005, August, 7",
-      area: 11500,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+        templeName: "Taipei Taiwan Temple",
+        location: "Taipei, Taipei, Taiwan",
+        dedicated: "1984, November, 17",
+        area: 9945,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/taipei-taiwan/400x250/taipei-taiwan-temple-lds-1031625-wallpaper.jpg"
     },
     {
-      templeName: "Manti Utah",
-      location: "Manti, Utah, United States",
-      dedicated: "1888, May, 21",
-      area: 74792,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        templeName: "Regina Sakatchewan Temple",
+        location: "Regina, Saskatchewan",
+        dedicated: "1999, Novenber, 14",
+        area: 10700,
+        imageUrl: 
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/regina-saskatchewan/400x250/regina-temple-lds-83522-wallpaper.jpg"
     },
     {
-      templeName: "Payson Utah",
-      location: "Payson, Utah, United States",
-      dedicated: "2015, June, 7",
-      area: 96630,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+        templeName: "Calgary Temple",
+        location: "Calgary, Alberta",
+        dedicated: "2012, October, 28",
+        area: 33000,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/calgary-alberta/400x250/calgary-albert-temple-lds-1059122-wallpaper.jpg"
     },
     {
-      templeName: "Yigo Guam",
-      location: "Yigo, Guam",
-      dedicated: "2020, May, 2",
-      area: 6861,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+        templeName: "Cardston Temple",
+        location: "Cardston, Alberta",
+        dedicated: "1923, August, 26",
+        area: 88562,
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/cardston-alberta/400x250/cardston-canada-temple-lds-1072764-wallpaper.jpg"
+
     },
     {
-      templeName: "Washington D.C.",
-      location: "Kensington, Maryland, United States",
-      dedicated: "1974, November, 19",
-      area: 156558,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+        templeName: "Salt Lake Temple",
+        location: "Salt Lake City, Utah, United States",
+        dedicated: "1893, April, 6",
+        area: 382207,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/salt-lake-city-utah/400x250/salt-lake-temple-37762.jpg"
+    },
+
+    {
+        templeName: "Idaho Falls Idaho Temple",
+        location: "Idaho Falls, Idaho",
+        dedicated: "23, September, 1945",
+        area: 41010,
+        imageUrl: "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/idaho-falls-idaho/2019/400x250/1-Idaho-Falls-Temple-2097425.jpg"
     },
     {
-      templeName: "Lima Perú",
-      location: "Lima, Perú",
-      dedicated: "1986, January, 10",
-      area: 9600,
-      imageUrl:
-      "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+        templeName: "Denver Colorado Temple",
+        location: "Denver, Colorado",
+        dedicated: "1986, October, 24",
+        area: 29117,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/denver-colorado/400x250/denver-temple-lds-766966-wallpaper.jpg"
+    },
+    
+    {
+        templeName: "Albuquerque New Mexico Temple",
+        location: "Albuquerque, New Mexico",
+        dedicated: "34245",
+        area: 6800,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/albuquerque-new-mexico/400x250/albuquerque-temple-lds-998219-wallpaper.jpg"
     },
     {
-      templeName: "Mexico City Mexico",
-      location: "Mexico City, Mexico",
-      dedicated: "1983, December, 2",
-      area: 116642,
-      imageUrl:"https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
-    },
-    // Add more temple objects here...
-    {templeName: "Vancouver British Columbia",
-    location: "Vancouver, Britisch Columbia, Canada",
-    dedicated: "2010, May, 2",
-    area: 28165,
-    imageUrl:
-    "https://www.victoriabuzz.com/wp-content/uploads/2024/04/mormon.png"
+        templeName: "Gilbert Arizona Temple",
+        location: "Gilbert, Arizona",
+        dedicated: "2014, March 2",
+        area: 85326,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/gilbert-arizona/400x250/gilbert-arizona-temple-exterior-1207309-wallpaper.jpg"
     },
     {
-    templeName: "Manila Philippines",
-    location: "Manila, Philippines",
-    dedicated: "1984, September, 25",
-    area: 26683,
-    imageUrl:
-    "https://newsroom.churchofjesuschrist.org/media/960x540/manilaphilippines_large.jpg"
+        templeName: "Phoenix Arizona Temple",
+        location: "Phoenix, Arizona",
+        dedicated: "2014, November, 2014",
+        area: 64870,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/phoenix-arizona/400x250/phoenix-arizona-temple-1284110-wallpaper.jpg"
     },
     {
-    templeName: "Taipei Taiwan",
-    location: "Taipei, Taiwan",
-    dedicated: "1984, Novenber, 17",
-    area: 9945,
-    imageUrl: 
-    "https://churchofjesuschristtemples.org/assets/img/temples/taipei-taiwan-temple/taipei-taiwan-temple-8302.jpg"
-    }
+        templeName: "Winnipeg Manitoba Temple",
+        location: "Winnipeg, Manitoba",
+        dedicated: "2021, October, 31",
+        area: 16100,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/winnipeg-manitoba/400x250/winnipeg_manitoba_temple_exterior.jpeg"
+    },
+    {
+        templeName: "Manila Philippines Temple",
+        location: "Manila, Philippines",
+        dedicated: "1984, September, 25",
+        area:26683,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manila-philippines/400x250/manila-philippines-temple-lds-129585-wallpaper.jpg"
+    },
+    {
+        templeName: "Portland Oregon Temple",
+        location: "Portlan, Oregon",
+        dedicated: "1989,August, 21",
+        area: 80500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/portland-oregon/2018/400x250/Portland-Oregon-Temple10.jpg"
+    },
+    {
+        templeName: "Seatle Washington Temple",
+        location: "Seatle, Washington",
+        dedicated: "1980, 21, November",
+        area: 11500,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/seattle-washington/400x250/seatlle-temple-lds-933559-wallpaper.jpg"
+    },
+    {
+        templeName: "Manti Utah",
+        location: "Manti, Utah, United States",
+        dedicated: "1888, May, 21",
+        area: 74792,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+    },
+    {
+        templeName: "Payson Utah",
+        location: "Payson, Utah, United States",
+        dedicated: "2015, June, 7",
+        area: 96630,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x250/payson-utah-temple-daylight-1416668-wallpaper.jpg"
+    },
+    {
+        templeName: "Yigo Guam",
+        location: "Yigo, Guam",
+        dedicated: "2020, May, 2",
+        area: 6861,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+    },
+    {
+        templeName: "Washington D.C.",
+        location: "Kensington, Maryland, United States",
+        dedicated: "1974, November, 19",
+        area: 156558,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+    },
+    {
+        templeName: "Lima Perú",
+        location: "Lima, Perú",
+        dedicated: "1986, January, 10",
+        area: 9600,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+    },
+    {
+        templeName: "St. George Utah Temple",
+        location: "St. George, Utah, United States",
+        dedicated: "1877, April, 6",
+        area: 143969,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/st-george-utah/400x250/st-george-utah-temple-clouds-922212-wallpaper.jpg"
+    },
+   
+    {
+        templeName: "Vancouver Canada Temple",
+        location: "Vancover, British Columbia ",
+        dedicated: "2010, May, 2",
+        area: 28165,
+        imageUrl:
+            "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/vancouver-british-columbia/400x250/vancouver-temple-766984-wallpaper.jpg"
+    },
+    
 ];
 
 let filteredTemples = [];
@@ -221,11 +314,11 @@ smallButton.addEventListener('click', () => {
     generate_temple_cards("small");
 });
 
+
+// Output the current year in the footer's first paragraph
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-const lastModifiedDate = new Date(document.lastModified);
-const options = {year: 'numeric', month: 'long', day:'numeric'};
-
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModifiedDate.toLocaleDateString (undefined, options)}`;
-document.getElementById("lastModified").textContent = `Last Modified: ${lastModifiedDate.toLocaleTimeString (undefined, options)}`;
+// Output the date the document was last modified in the footer's second paragraph
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
